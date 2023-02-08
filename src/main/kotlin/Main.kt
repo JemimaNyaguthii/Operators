a73d6d6d696954fdbe3d786c8780df253eb95fdf
JemimaNyaguthii <jemimanyaguthiiwambugu@gmail.com>
2023-02-08 16:43:33 +0300

    submit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,37 @@
+fun main(){
+var name= "Jem"
+    println("Hello,$name")
+    modulus(60,120)
+    modulus(540,720)
+
+    addition(40,60)
+    addition(35,15)
+    addition(550,670)
+    addition(1998,2004)
+
+    interestingFact("Jemima,isaGem")
+
+}
+fun modulus(num1:Int,num2:Int){
+    var result = (num1 % num2)
+    println(result)
+
+}
+fun addition(num1:Int, num2:Int){
+    var sum =num1 + num2
+    println(sum)
+
+}
+fun interestingFact(name:String){
+    var interesting=("Jemima,isagem")
+    println(interesting)
+
+}
+
+
+
+
+
+
+
+
